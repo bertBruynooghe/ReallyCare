d8c3a9dfb679baa5022ca780beeb33354ad15430
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2017-12-11 18:44:11 +0100

    added trend

--- thetest.js
+++ thetest.js
@@ -17,6 +17,7 @@ http.createServer(function (ignore, response) {
         });
         var message = '<strong>' + data.lastSG.sg + '</strong><br>';
         message += data.lastSG.datetime + '<br>';
+        message += '<dl><dt>trend<dd>' + data.lastSGTrend + '</dl>';
         message += JSON.stringify(data, 2);
         response.end(message);
     });
