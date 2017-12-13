e4c1f21cf5fd9d2269439924b601f22ec3f1b83f
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2017-12-13 15:45:13 +0100

    added more information to the GUI

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ var router = express.Router();
 var carelink = require('./../carelink');
 var client = carelink.Client({username: process.env.USERNAME,
         password: process.env.PASSWORD});
+var moment = require('moment');
+moment.locale('nl-be');
 
 /* GET home page. */
 router.get('/', function (ignore, res) {
     'use strict';
     client.fetch(function callback(ignore, data) {
-        res.render('index', {data: data});
+        res.render('index', {data: data, moment: moment});
     });
 });
 
